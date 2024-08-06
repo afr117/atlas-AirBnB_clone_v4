715416172ba4bab8e1c7d6ec447f3cc9133f29ec
kevxcyj <kevcyjunior@gmail.com>
2024-08-06 01:41:07 -0500

    Added the files for number 1, Completed the script for 1-js, and the files for 1-py & 1-html

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    var selectedAmenities = {};
+
+    $('input[type="checkbox"]').change(function() {
+        const amenityId = $(this).data('id');
+        const amenityName = $(this).data('name');
+
+        if ($(this).is(':checked')) {
+            selectedAmenities[amenityId] = amenityName;
+        } else {
+            delete selectedAmenities[amenityId];
+        }
+
+        updateAmenitiesDisplay();
+    });
+
+    function updateAmenitiesDisplay() {
+        const amenitiesDiv = $('#amenities');
+        const amenitiesList = Object.entries(selectedAmenities).map(([id, name]) => `${id}: ${name}`).join(', ');
+        amenitiesDiv.find('h4').text(`Amenities (${Object.keys(selectedAmenities).length}): ${amenitiesList}`);
+    }
+});
