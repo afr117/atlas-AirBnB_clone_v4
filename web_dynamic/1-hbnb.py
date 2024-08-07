ec5192a47c6a8234365fba9f9d126d1fcc984a49
afr117 <alfredo.figueroa07@yahoo.com>
2024-08-07 18:03:42 +0000

    layout edit running correctlysql

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -1,12 +1,43 @@
+#!/usr/bin/python3
+""" Starts a Flash Web Application """
 from flask import Flask, render_template
 import uuid
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
 
 app = Flask(__name__)
 
-@app.route('/1-hbnb/')
-def hbnb_1():
+@app.teardown_appcontext
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+@app.route('/1-hbnb/', strict_slashes=False)
+def hbnb():
+    """ HBNB is alive! """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    st_ct = []
+
+    for state in states:
+        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
+
+    amenities = storage.all(Amenity).values()
+    amenities = sorted(amenities, key=lambda k: k.name)
+
+    places = storage.all(Place).values()
+    places = sorted(places, key=lambda k: k.name)
+
     cache_id = str(uuid.uuid4())
-    return render_template('1-hbnb.html', cache_id=cache_id)
+
+    return render_template('1-hbnb.html',
+                           states=st_ct,
+                           amenities=amenities,
+                           places=places,
+                           cache_id=cache_id)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
